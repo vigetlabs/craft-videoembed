67e6841ce959d37c8e0e346e7eecf96331bc3eaf
Thomas De Marez <thomasdm@glue.be>
2026-05-08 09:22:27 +0200

    fix vimeo video’s with hash not generating a valid embedUrl

--- tests/ParsingHelperTest.php
+++ tests/ParsingHelperTest.php
@@ -36,4 +36,71 @@ final class ParsingHelperTest extends TestCase
             ParsingHelper::getYouTubeIdFromUrl('https://youtube.com/watch?v=--HXLM8GuxA')
         );
     }
+
+    public function testGetVimeoIdFromUrl(): void
+    {
+        $this->assertEquals(
+            '9999999999',
+            ParsingHelper::getVimeoIdFromUrl('https://vimeo.com/9999999999')
+        );
+
+        $this->assertEquals(
+            '9999999999',
+            ParsingHelper::getVimeoIdFromUrl('https://vimeo.com/9999999999/0000000000')
+        );
+
+        $this->assertEquals(
+            '9999999999',
+            ParsingHelper::getVimeoIdFromUrl('https://player.vimeo.com/video/9999999999')
+        );
+
+        $this->assertEquals(
+            '9999999999',
+            ParsingHelper::getVimeoIdFromUrl('https://player.vimeo.com/video/9999999999?h=0000000000')
+        );
+    }
+
+    public function testGetVimeoHashFromUrl(): void
+    {
+        $this->assertNull(
+            ParsingHelper::getVimeoHashFromUrl('https://vimeo.com/9999999999')
+        );
+
+        $this->assertEquals(
+            '0000000000',
+            ParsingHelper::getVimeoHashFromUrl('https://vimeo.com/9999999999/0000000000')
+        );
+
+        $this->assertNull(
+            ParsingHelper::getVimeoHashFromUrl('https://player.vimeo.com/video/9999999999')
+        );
+
+        $this->assertEquals(
+            '0000000000',
+            ParsingHelper::getVimeoHashFromUrl('https://player.vimeo.com/video/9999999999?h=0000000000')
+        );
+    }
+
+    public function testGetVimeoEmbedUrl(): void
+    {
+        $this->assertEquals(
+            'https://player.vimeo.com/video/9999999999',
+            ParsingHelper::getVideoDataFromUrl('https://vimeo.com/9999999999')->embedUrl
+        );
+
+        $this->assertEquals(
+            'https://player.vimeo.com/video/9999999999?h=0000000000',
+            ParsingHelper::getVideoDataFromUrl('https://vimeo.com/9999999999/0000000000')->embedUrl
+        );
+
+        $this->assertEquals(
+            'https://player.vimeo.com/video/9999999999',
+            ParsingHelper::getVideoDataFromUrl('https://player.vimeo.com/video/9999999999')->embedUrl
+        );
+
+        $this->assertEquals(
+            'https://player.vimeo.com/video/9999999999?h=0000000000',
+            ParsingHelper::getVideoDataFromUrl('https://player.vimeo.com/video/9999999999?h=0000000000')->embedUrl
+        );
+    }
 }
